302036f4f5e388c2e8c4e94004d7b8f129169826
John Topley <john.topley@ons.gov.uk>
2017-03-03 12:26:52 +0000

    CTPA-1017 Response Management Git commit SHA should come from the AWS branch rather than master

--- response-management.sh
+++ response-management.sh
@@ -7,5 +7,5 @@
 #
 # Author: John Topley (john.topley@ons.gov.uk)
 #
-export RM_GIT_COMMIT=$(cd /home/centos/code/response-management-service && git rev-parse --short HEAD)
+export RM_GIT_COMMIT=$(cd /home/centos/code/response-management-service && git rev-parse --short aws)
 packer build -var-file=./aws-variables.json -var-file=./response-management/variables.json ./response-management/template.json
